1539472f41a906c86ccec25cfdd5af2eb1bd7092
Tony Dandino <tdandino1005@gmail.com>
2023-03-05 20:01:50 -0600

    Fixed schema.sql by adding foreign key for manager

--- db/schema.sql
+++ db/schema.sql
@@ -25,10 +25,11 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT REFERENCES employee(id)
-    ON DELETE SET NULL,
+    role_id INT NOT NULL,
+    manager_id INT REFERENCES employee(id),
     FOREIGN KEY (role_id)
     REFERENCES role(id)
-    ON DELETE SET NULL
+    manager_id INT,
+   FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
 );
